0ded8c558b4f2b04fc2de41f5daf1b1356458262
GUI SIEBERT <guilhermebsiebert@gmail.com>
2023-01-28 20:22:05 -0300

    add temporary logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import './styles/global.css';
 
-import { Habit } from "./components/Habit"
+// import { Habit } from "./components/Habit"
+import logoImage from './assets/svglogo.svg'
 
-function App() {
+
+export function App() {
   return (
-    <div>
-      <Habit completed={15} />
-      <Habit completed={33} />
-      <Habit completed={5} />
-      <Habit completed={11} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt='habits' />
+
+          <button 
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg pz-6 py-4'
+            >
+            New Habit
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
-
-export default App
-
